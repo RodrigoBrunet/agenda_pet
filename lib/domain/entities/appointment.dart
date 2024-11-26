c425d4f53a40fdd25e76db447e5a570c5d34621e
Rodrigo Brunet <rodrigosistemas2016@gmail.com>
2024-11-26 17:13:58 -0300

    Cria entidades em domain

---
+++ lib/domain/entities/appointment.dart
@@ -0,0 +1,13 @@
+class Appointment {
+  final int id;
+  final int petId;
+  final DateTime date;
+  final String service;
+
+  Appointment({
+    required this.id,
+    required this.petId,
+    required this.date,
+    required this.service,
+  });
+}
---
+++ lib/domain/entities/pet.dart
@@ -0,0 +1,7 @@
+class Pet {
+  final int id;
+  final String name;
+  final String breed;
+
+  Pet({required this.id, required this.name, required this.breed});
+}
